9f8d6eedd743396174214511b68a589b9aa44c63
RynzDev <101009914+RynzDev@users.noreply.github.com>
2022-04-01 07:52:49 +0700

    Add files via upload

--- src/RynzDev/SimpleGM/Main.php
+++ src/RynzDev/SimpleGM/Main.php
@@ -1,1 +1,27 @@
+<?php
 
+namespace RynzDev\SimpleGM;
+
+use RynzDev\SimpleGM\commands\gms;
+use RynzDev\SimpleGM\commands\gmc;
+use RynzDev\SimpleGM\commands\gma;
+use RynzDev\SimpleGM\commands\gmspc;
+use pocketmine\command\Command;
+use pocketmine\event\Listener;
+use pocketmine\plugin\PluginBase;
+use pocketmine\tile\Tile;
+use pocketmine\utils\Config;
+use pocketmine\utils\TextFormat;
+use pocketmine\Server;
+use pocketmine\player\Player;
+
+class Main extends PluginBase {
+    public $config;
+
+    public function onEnable(): void {
+        $this->getServer()->getCommandMap()->register("gms", new gms($this));
+        $this->getServer()->getCommandMap()->register("gmc", new gmc($this));
+        $this->getServer()->getCommandMap()->register("gma", new gma($this));
+        $this->getServer()->getCommandMap()->register("gmspc", new gmspc($this));
+    }
+}
