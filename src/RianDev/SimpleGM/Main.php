6b26624d8336d7abd261db46db65f9c8fc31f205
RianDev <101009914+rmlnae@users.noreply.github.com>
2024-01-29 21:36:41 +0700

    Add files via upload

--- src/RianDev/SimpleGM/Main.php
+++ src/RianDev/SimpleGM/Main.php
@@ -1,1 +1,27 @@
+<?php
 
+namespace RianDev\SimpleGM;
+
+use RianDev\SimpleGM\commands\gms;
+use RianDev\SimpleGM\commands\gmc;
+use RianDev\SimpleGM\commands\gma;
+use RianDev\SimpleGM\commands\gmspc;
+use pocketmine\command\Command;
+use pocketmine\event\Listener;
+use pocketmine\plugin\PluginBase;
+use pocketmine\tile\Tile;
+use pocketmine\utils\Config;
+use pocketmine\utils\TextFormat;
+use pocketmine\Server;
+use pocketmine\player\Player;
+
+class Main extends PluginBase {
+    public $config;
+
+    public function onEnable(): void {
+        $this->getServer()->getCommandMap()->register("gms", new gms($this));
+        $this->getServer()->getCommandMap()->register("gmc", new gmc($this));
+        $this->getServer()->getCommandMap()->register("gma", new gma($this));
+        $this->getServer()->getCommandMap()->register("gmspc", new gmspc($this));
+    }
+}
